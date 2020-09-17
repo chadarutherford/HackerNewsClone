c1ee3544c074ca0b8f0d263b0c38adedced7b15c
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-17 13:13:05 -0400

    Added Testing Framework and Removed Main.storyboard from plist

---
+++ HackerNewsCloneTests/HackerNewsCloneJSONTests.swift
@@ -0,0 +1,12 @@
+//
+//  HackerNewsCloneJSONTests.swift
+//  HackerNewsCloneTests
+//
+//  Created by Chad Rutherford on 9/17/20.
+//
+
+import XCTest
+@testable import HackerNewsClone
+
+class HackerNewsCloneJSONTests: XCTestCase {
+}
